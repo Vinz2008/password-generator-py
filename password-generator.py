582091fdac84737f88fafefffc9717900c43e205
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-04 11:13:33 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
 import random
+passlength = input("How much character do you want in your password ?")
+passlength = int(passlength)
+answerCharsNb = input("Do you want numbers in your password ? (y/n)")
+if answerCharsNb == "y" :
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()?"
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()?"
-passlength = 16
+passlength
 password =  "".join(random.sample(chars,passlength ))
 print (password)
