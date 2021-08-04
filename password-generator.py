001002892029dd84c4786020607171dc3f388e77
Vinz2008 <68145293+Vinz2008@users.noreply.github.com>
2021-08-04 12:29:34 +0200

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -5,8 +5,10 @@ passlength = input("How much character do you want in your password ?")
 passlength = int(passlength)
 answerCharsNb = input("Do you want numbers in your password ? (y/n)")
 answerCharsSpecial = input("Do you want special characters in your password ? (y/n)")
+answerCharsCapital = input("Do you want Capital letters in your password ? (y/n)")
 if answerCharsNb == "y":
     if answerCharsSpecial =="y":
+        if answerCharsCapital == " y":
 
     if answerCharSpecial == "n":    
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890!@#$%^&*()?"
